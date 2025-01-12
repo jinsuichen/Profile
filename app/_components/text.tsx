fdb2d07c5764a5c2576f1e2e040e5f7165203862
Jinsui Chen <ischenjinsui@gmail.com>
2025-01-12 14:54:58 +0800

    feat(app/page.tsx): home page

---
+++ app/_components/text.tsx
@@ -0,0 +1,32 @@
+import localFont from "next/font/local";
+import React from "react";
+
+const alibabaHeavy = localFont({
+    src: '../../public/font/heavy.ttf'
+});
+
+const alibabaRegular = localFont({
+    src: '../../public/font/regular.ttf'
+});
+
+// Define the type styles
+const typeStyles = {
+    p: { tag: 'p', className: `${alibabaRegular.className} text-base` },
+    h1: { tag: 'h1', className: `${alibabaHeavy.className} text-4xl` },
+    h2: { tag: 'h2', className: `${alibabaHeavy.className} text-3xl` },
+    h3: { tag: 'h3', className: `${alibabaHeavy.className} text-2xl` },
+    h4: { tag: 'h4', className: `${alibabaHeavy.className} text-xl` },
+    'special-huge': { tag: 'h1', className: `${alibabaHeavy.className} text-7xl` },
+    'special-title': { tag: 'h2', className: `${alibabaHeavy.className} text-4xl` },
+    'special-text': { tag: 'p', className: `${alibabaRegular.className} text-2xl font-normal` },
+};
+
+export default function Text({ children, type = 'p', className = '', ...props }) {
+    const style = typeStyles[type] || typeStyles.p; // default to p if type is not found
+
+    return React.createElement(
+        style.tag,
+        { className: `${style.className} ${className}`, ...props },
+        children
+    );
+}
