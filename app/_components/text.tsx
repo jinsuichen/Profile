204e3e4dd506e270a142a39e4ae0c7be593bbd1f
Jinsui Chen <ischenjinsui@gmail.com>
2025-01-12 15:03:55 +0800

    feat(Text component): Define the props

--- app/_components/text.tsx
+++ app/_components/text.tsx
@@ -21,6 +21,15 @@ const typeStyles = {
     'special-text': { tag: 'p', className: `${alibabaRegular.className} text-2xl font-normal` },
 };
 
+// Define the props
+interface TextProps {
+    children?: React.ReactNode;
+    type?: keyof typeof typeStyles;
+    className?: string;
+
+    [key: string]: any; // allow any other props
+}
+
 export default function Text({ children, type = 'p', className = '', ...props }) {
     const style = typeStyles[type] || typeStyles.p; // default to p if type is not found
 
